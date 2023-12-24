be2e032c58e0dce5043e46aa116f6a3d1d8c22cb
Renan <renanrgarcia@gmail.com>
2023-12-24 15:37:51 -0300

    Aulas 48.2

--- aula48.py
+++ aula48.py
@@ -4,19 +4,18 @@ Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis:
-    append, insert, pop, del, clear, extend, +
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
 Create Read Update   Delete
 Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
-#        0   1   2   3   4   5
-lista = [10, 20, 30, 40]
-# lista[2] = 300
-# del lista[2]
-# print(lista)
-# print(lista[2])
-lista.append(50)
-lista.pop()
-lista.append(60)
-lista.append(70)
-ultimo_valor = lista.pop(3)
-print(lista, 'Removido,', ultimo_valor)
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
