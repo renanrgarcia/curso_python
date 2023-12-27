4b6357d284c6f7fae45ddbd43aa9f6494de48b57
Renan <renanrgarcia@gmail.com>
2023-12-27 08:30:11 -0300

    Aula 76.3

--- aula76.py
+++ aula76.py
@@ -1,26 +1,30 @@
-# Manipulando chaves e valores em dicionários
-pessoa = {}
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda 3',
+    # 'idade': 900
+}
 
-##
-##
+pessoa.setdefault('idade', 0)
+print(pessoa['idade'])
+# print(len(pessoa))
+# print(list(pessoa.keys()))
+# print(list(pessoa.values()))
+# print(list(pessoa.items()))
 
-chave = 'nome'
 
-pessoa[chave] = 'Renan'
-pessoa['sobrenome'] = 'Reis Garcia'
+# for chave in pessoa.values():
+#     print(chave)
 
-print(pessoa[chave])
-
-pessoa[chave] = 'Thaís'
-
-del pessoa['sobrenome']
-print(pessoa)
-print(pessoa['nome'])
-
-# print(pessoa.get('sobrenome'))
-if pessoa.get('sobrenome') is None:
-    print('Não EXISTE')
-else:
-    print(pessoa['sobrenome'])
-
-# print('ISSO Não vai')
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
