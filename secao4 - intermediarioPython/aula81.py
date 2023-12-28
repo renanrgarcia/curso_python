6ca46018d951629cf711c19392a5835bccadf9f6
Renan <renanrgarcia@gmail.com>
2023-12-28 19:25:32 -0300

    Aulas 81-84

--- secao4 - intermediarioPython/aula81.py
+++ secao4 - intermediarioPython/aula81.py
@@ -11,8 +11,27 @@
 #     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
 #     {'nome': 'Aline', 'sobrenome': 'Souza'},
 # ]
-lista = [4, 32, 1, 34, 5, 6, 6, 21,]
-lista.sort()
+# lista = [4, 32, 1, 34, 5, 6, 6, 21,]
+# lista.sort(reverse=True)
 # sorted(lista)
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'Miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
 
-print(lista)
+# lista.sort(key=lambda item: item['nome'])
+
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
