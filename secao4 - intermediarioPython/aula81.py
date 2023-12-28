7b94298a64aaa87f00dabfc3dec5508639be49f5
Renan <renanrgarcia@gmail.com>
2023-12-28 09:55:05 -0300

    Created folders by sections

---
+++ secao4 - intermediarioPython/aula81.py
@@ -0,0 +1,18 @@
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
+# lista = [
+#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
+lista = [4, 32, 1, 34, 5, 6, 6, 21,]
+lista.sort()
+# sorted(lista)
+
+print(lista)
