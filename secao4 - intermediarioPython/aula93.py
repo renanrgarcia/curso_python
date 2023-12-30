405948b579a5a89f51ea1914aa7751a070502d20
Renan <renanrgarcia@gmail.com>
2023-12-30 06:21:13 -0300

    Aula 93.2

--- secao4 - intermediarioPython/aula93.py
+++ secao4 - intermediarioPython/aula93.py
@@ -6,15 +6,19 @@
 try:
     a = 18
     b = 0
-    print('Linha 1')
+    # print(b[0])
+    # print('Linha 1'[1000])
     c = a / b
     print('Linha 2')
-except ZeroDivisionError:
-    print('Dividiu por zero.')
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
 except NameError:
     print('Nome b não está definido.')
-except (TypeError, IndexError):
+except (TypeError, IndexError) as error:
     print('TypeError + IndexError')
+    print('MSG:', error)
+    print('Nome', error.__class__.__name__)
 except Exception:
     print('ERRO DESCONHECIDO.')
 print('CONTINUAR')
