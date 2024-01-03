84038f156e4f83c8c453359cd36a9e6f6c4ebd4e
Renan <renanrgarcia@gmail.com>
2024-01-03 17:55:15 -0300

    Aulas 111-112

--- secao4 - intermediarioPython/aula111.py
+++ secao4 - intermediarioPython/aula111.py
@@ -16,4 +16,53 @@ produtos = [
     {'nome': 'Produto 3', 'preco': 10.11},
     {'nome': 'Produto 2', 'preco': 105.87},
     {'nome': 'Produto 4', 'preco': 69.90},
-] 
+]
+
+
+def aumentar_porcentagem(valor, porcentagem):
+    return round(valor * porcentagem)
+
+
+aumentar_dez_porcento = partial(
+    aumentar_porcentagem,
+    porcentagem=1.1
+)
+
+# novos_produtos = [
+#     {**p,
+#         'preco': aumentar_dez_porcento(p['preco'])}
+#     for p in produtos
+# ]
+
+
+def muda_preco_de_produto(produto):
+    return {
+        **produto,
+        'preco': aumentar_dez_porcento(
+            produto['preco']
+        )
+    }
+
+
+novos_produtos = list(map(
+    muda_preco_de_produto,
+    produtos
+))
+
+novos_produtos = (x for x in produtos)  # generator
+
+
+print_iter(produtos)
+print_iter(novos_produtos)
+
+# print(novos_produtos)
+# print(hasattr(novos_produtos, '__iter__'))
+# print(hasattr(novos_produtos, '__next__'))
+# print(isinstance(novos_produtos, GeneratorType))
+
+print(
+    list(map(
+        lambda x: x * 3,
+        [1, 2, 3, 4]
+    ))
+)
