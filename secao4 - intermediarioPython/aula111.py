bdc40e5ee13d9d78d9f324a284de71a5b2164f23
Renan <renanrgarcia@gmail.com>
2024-01-03 15:43:01 -0300

    Aulas 108-111

---
+++ secao4 - intermediarioPython/aula111.py
@@ -0,0 +1,19 @@
+# map, partial, GeneratorType e esgotamento de Iterators
+
+from functools import partial
+from types import GeneratorType
+
+
+# map - para mapear dados
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+] 
