3e406b74daa36ff88b244ccb6c235d42b7fc60fb
Renan <renanrgarcia@gmail.com>
2024-01-03 10:44:49 -0300

    Aula 107.2

--- secao4 - intermediarioPython/aula107.2.py
+++ secao4 - intermediarioPython/aula107.2.py
@@ -9,6 +9,32 @@ lista_b     = [1, 2, 3, 4]
 =================== resultado
 lista_soma  = [2, 4, 6, 8]
 """
-
-lista_a = [10, 2, 3, 40, 5, 6, 7]
+lista_a = [1, 2, 3, 4, 5, 6, 7]
 lista_b = [1, 2, 3, 4]
+
+# Minha solução
+# if len(lista_a) > len(lista_b):
+#     for i in range(len(lista_b)):
+#         lista_soma.append(lista_a[i] + lista_b[i])
+# else:
+#     for i in range(len(lista_a)):
+#         lista_soma.append(lista_a[i] + lista_b[i])
+# print(lista_soma)
+
+# Minha solução otimizada
+# lista_soma = [
+#     lista_a[i] + lista_b[i]
+#     for i in range(min(len(lista_a), len(lista_b)))
+# ]
+
+# print(lista_soma)
+
+# Solução 1 
+# lista_soma = []
+# for i, _ in enumerate(lista_b):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+# print(lista_soma)
+
+# Solução 2
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_soma)
