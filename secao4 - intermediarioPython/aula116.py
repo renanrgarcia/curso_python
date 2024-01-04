4731d840265b43886fb02bbd12155e2fd31ed9e5
Renan <renanrgarcia@gmail.com>
2024-01-04 13:11:12 -0300

    Aulas 113, 114 e 116

--- secao4 - intermediarioPython/aula116.py
+++ secao4 - intermediarioPython/aula116.py
@@ -18,7 +18,9 @@
 # Vamos falar mais sobre o módulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
-caminho_arquivo = r'D:\projects\curso_python\secao4 - intermediarioPython\aula116.txt'
+import os
+
+caminho_arquivo = 'D:\\projects\\curso_python\\secao4 - intermediarioPython\\aula116.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
 
@@ -50,7 +52,12 @@ caminho_arquivo = r'D:\projects\curso_python\secao4 - intermediarioPython\aula11
 
 with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
     arquivo.write('Atenção\n')
+    arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
     arquivo.writelines(
         ('Linha 3\n', 'Linha 4\n')
     )
+
+# os.remove(caminho_arquivo)
+os.rename(caminho_arquivo,
+          'D:\\projects\\curso_python\\secao4 - intermediarioPython\\aula116-2.txt')
