20e87c1e1be381e342c3a6cd5556c7950836648d
Renan <renanrgarcia@gmail.com>
2024-01-12 18:32:45 -0300

    DocStrings em class (Documentação)

--- secao5-introducao_a_POO_com_Python-Classes/aula156/main.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula156/main.py
@@ -1,7 +1,7 @@
-import documentacao_funcoes as docFunc
+import documentacao_class
 
 # print(dir(varias_linhas))
 # print(varias_linhas.__doc__)
 # print(varias_linhas.__file__)
 # print(varias_linhas.__name__)
-help(docFunc)
+help(documentacao_class)
