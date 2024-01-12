1984aad383c3a101f038b2cfd213f7f804da4d7d
Renan <renanrgarcia@gmail.com>
2024-01-12 12:29:05 -0300

    Código: Metaclasses são o tipo das classes

--- secao5-introducao_a_POO_com_Python-Classes/aula155.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula155.py
@@ -22,13 +22,47 @@
 # com certeza que precisam delas e não precisam de uma explicação
 # sobre o porquê)."
 # — Tim Peters (CPython Core Developer)
+def meu_repr(self):
+    return f'{type(self).__name__}({self.__dict__})'
 
-# object acima
-# class Foo:
-#     ...
 
-Foo = type('Foo', (object,), {})
-f = Foo()
-# print(isinstance(f, Foo))
-print(type(f))
-print(type(Foo))
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        print('METACLASS NEW')
+        cls = super().__new__(mcs, name, bases, dct)
+        cls.attr = 1234
+        cls.__repr__ = meu_repr
+
+        if 'falar' not in cls.__dict__ or \
+                not callable(cls.__dict__['falar']):
+            raise NotImplementedError('Implemente o método falar')
+
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        instancia = super().__call__(*args, **kwargs)
+
+        if instancia not in instancia.__dict__:
+            raise NotImplementedError('Crie o attr nome')
+
+        return instancia
+
+
+class Pessoa(metaclass=Meta):
+    # falar = 123
+
+    def __new__(cls, *args, **kwargs):
+        print('MEU NEW')
+        instancia = super().__new__(cls)
+        return instancia
+
+    def __init__(self, nome):
+        print('MEU INIT')
+        # self.nome = nome
+
+    def falar(self):
+        print('Falando...')
+
+
+p1 = Pessoa('Joaquim')
+p1.falar()
