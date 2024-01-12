4f1be25818103326641136b86cf0207751e57e7e
Renan <renanrgarcia@gmail.com>
2024-01-12 18:16:35 -0300

    DocStrings de várias linhas (Documentação)

--- secao5-introducao_a_POO_com_Python-Classes/aula156/main.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula156/main.py
@@ -1,7 +1,7 @@
-import uma_linha
+import varias_linhas
 
-print(dir(uma_linha))
-print(uma_linha.__doc__)
-print(uma_linha.__file__)
-print(uma_linha.__name__)
-help(uma_linha)
+print(dir(varias_linhas))
+print(varias_linhas.__doc__)
+print(varias_linhas.__file__)
+print(varias_linhas.__name__)
+help(varias_linhas)
