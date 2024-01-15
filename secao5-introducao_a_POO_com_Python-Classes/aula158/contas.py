db4263c1e65f31d6f4d9b5114a029eadd467bd9e
Renan <renanrgarcia@gmail.com>
2024-01-15 18:18:13 -0300

    Extra: tipagem, linters e settings.json do VS Code

--- secao5-introducao_a_POO_com_Python-Classes/aula158/contas.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula158/contas.py
@@ -2,19 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class Conta (ABC):
-    def __init__(self, agencia, conta, saldo=0.00):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
     @abstractmethod
-    def sacar(self, valor): ...
+    def sacar(self, valor: float) -> float: ...
 
-    def depositar(self, valor):
+    def depositar(self, valor: float) -> float:
         self.saldo += valor
         self.detalhes(f'(DEPÓSITO {valor})')
+        return self.saldo
 
-    def detalhes(self, msg):
+    def detalhes(self, msg: str = '') -> None:
         print(f'O seu saldo é: {self.saldo:.2f} {msg}')
         print('--')
 
@@ -30,14 +31,18 @@ class ContaPoupanca(Conta):
         print(f'Não foi possível sacar {valor}')
         print('Seu limite é 0.00')
         self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
 
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia, conta, saldo=0.00, limite=0.00):
+    def __init__(
+        self, agencia: int, conta: int,
+        saldo: float = 0.00, limite=0.00
+    ):
         super().__init__(agencia, conta, saldo)
         self.limite = limite
 
-    def sacar(self, valor):
+    def sacar(self, valor: float) -> float:
         valor_pos_saque = self.saldo - valor
         limite_maximo = -self.limite
         if valor_pos_saque >= limite_maximo:
@@ -48,6 +53,7 @@ class ContaCorrente(Conta):
         print(f'Não foi possível sacar {valor}')
         print(f'Seu limite é {-self.limite:.2f}')
         self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
 
 
 if __name__ == '__main__':
