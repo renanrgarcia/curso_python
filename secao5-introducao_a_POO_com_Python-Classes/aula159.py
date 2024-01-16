6da144acbde7baadfb303d7d93d04b16de691dd4
Renan <renanrgarcia@gmail.com>
2024-01-16 17:18:20 -0300

    __init__ e __post_init__ em dataclasses

--- secao5-introducao_a_POO_com_Python-Classes/aula159.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula159.py
@@ -8,24 +8,33 @@
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(init=False)
 class Pessoa:
     nome: str
     sobrenome: str
 
-    @property
-    def nome_completo(self):
-        return f'{self.nome} {self.sobrenome}'
-
-    @nome_completo.setter
-    def nome_completo(self, valor):
-        nome, *sobrenome = valor.split()
+    def __init__(self, nome, sobrenome):
         self.nome = nome
-        self.sobrenome = ' '.join(sobrenome)
+        self.sobrenome = sobrenome
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+    def __post_init__(self):
+        print('POST INIT')
+    # def __post_init__(self):
+    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
+
+    # @nome_completo.setter
+    # def nome_completo(self, valor):
+    #     nome, *sobrenome = valor.split()
+    #     self.nome = nome
+    #     self.sobrenome = ' '.join(sobrenome)
 
 
 if __name__ == '__main__':
     p1 = Pessoa('Renan', 'Garcia')
-    p1.nome_completo = 'Renan Reis Garcia'
     print(p1)
     print(p1.nome_completo)
