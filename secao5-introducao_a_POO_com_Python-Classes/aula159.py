6dacc9585c8cd69ca24845caac48dfd42da7c584
Renan <renanrgarcia@gmail.com>
2024-01-16 17:50:11 -0300

    Valores padrão e field em dataclasses

--- secao5-introducao_a_POO_com_Python-Classes/aula159.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula159.py
@@ -5,18 +5,20 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass, asdict, astuple
+from dataclasses import dataclass, field, fields
 
 
 @dataclass(repr=True)
 class Pessoa:
-    nome: str
-    sobrenome: str
+    nome: str = field(
+        default='Missing',
+    )
+    sobrenome: str = 'Not send'
+    idade: int = 100
+    enderecos: list[str] = field(default_factory=list)
 
 
 if __name__ == '__main__':
     p1 = Pessoa('Renan', 'Garcia')
-    print(asdict(p1).keys())
-    print(asdict(p1).values())
-    print(asdict(p1).items())
-    print(astuple(p1)[0])
+    # print(fields(p1))
+    print(p1)
