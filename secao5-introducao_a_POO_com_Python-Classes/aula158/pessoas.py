bfcba16fdbfc78204117cfc5bbc8811b5bdff082
Renan Garcia <renanrgarcia@gmail.com>
2024-01-16 08:52:53 -0300

    Adjusts class Pessoas

--- secao5-introducao_a_POO_com_Python-Classes/aula158/pessoas.py
+++ secao5-introducao_a_POO_com_Python-Classes/aula158/pessoas.py
@@ -1,29 +1,43 @@
-from abc import ABC
-import contas
+from abc import ABC, abstractmethod
+from contas import ContaCorrente, ContaPoupanca
 
 
 class Pessoa(ABC):
     def __init__(self, nome, idade) -> None:
         self._nome = nome
         self._idade = idade
-        self.conta = contas.Conta
 
     @property
-    def nome(self):
-        return self._nome
+    @abstractmethod
+    def nome(self): ...
 
     @nome.setter
-    def nome(self, valor):
-        self._nome = valor
+    @abstractmethod
+    def nome(self, valor): ...
 
     @property
-    def idade(self):
-        return self._idade
+    @abstractmethod
+    def idade(self): ...
 
     @idade.setter
+    @abstractmethod
     def idade(self, valor):
         self._idade = valor
 
 
 class Cliente(Pessoa):
-    ...
+    def __init__(
+        self, nome: str, idade: int,
+        conta_poupanca: ContaPoupanca, conta_corrente: ContaCorrente
+    ) -> None:
+        super().__init__(nome, idade)
+        self.conta_poupanca = conta_poupanca
+        self.conta_corrente = conta_corrente
+
+    @Pessoa.nome.setter
+    def nome(self, valor):
+        self.nome = valor
+
+
+if __name__ == '__main__':
+    c1 = Cliente('Renan', 31, (123, 123, 10), (321, 321, 10, 10))
