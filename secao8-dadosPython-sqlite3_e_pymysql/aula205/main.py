339749b2c2969a3c0ac5bbc7c2d490141998c1ab
Renan <renanrgarcia@gmail.com>
2024-01-17 13:20:55 -0300

    Initial commit - secao19

--- secao8-dadosPython-sqlite3_e_pymysql/aula205/main.py
+++ secao8-dadosPython-sqlite3_e_pymysql/aula205/main.py
@@ -4,12 +4,20 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-# SQL
-
+cursor.execute(
+    F'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
