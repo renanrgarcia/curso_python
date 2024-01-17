1bdbef1bbf8b31e717a4f741d3c75ed565a52d46
Renan Garcia <renanrgarcia@gmail.com>
2024-01-17 20:00:16 -0300

    DELETE no SQLite do Python

--- secao8-dadosPython-sqlite3_e_pymysql/aula205/main.py
+++ secao8-dadosPython-sqlite3_e_pymysql/aula205/main.py
@@ -66,12 +66,22 @@ if __name__ == '__main__':
     print(sql)
 
     cursor.execute(
-    f'SELECT * FROM {TABLE_NAME}'
+        f'DELETE FROM {TABLE_NAME} '
+        'WHERE id = "3"'
+    )
+    cursor.execute(
+        f'DELETE FROM {TABLE_NAME} '
+        'WHERE id = 1'
+    )
+    connection.commit()
+
+    cursor.execute(
+        f'SELECT * FROM {TABLE_NAME}'
     )
 
     for row in cursor.fetchall():
-    _id, name, weight = row
-    print(_id, name, weight)
+        _id, name, weight = row
+        print(_id, name, weight)
 
     cursor.close()
     connection.close()
