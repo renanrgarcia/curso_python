a3849be64e23b850c8a2464f31d4e9011aa914e3
Renan <renanrgarcia@gmail.com>
2024-01-23 17:26:14 -0300

    Renderizando HTML, render, templates, INSTALLED_APPS e TemplateDoesNotExist

--- secao9-djangoNoPython/aula127_ola_django/home/views.py
+++ secao9-djangoNoPython/aula127_ola_django/home/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
     print('home')
-    return HttpResponse('home do app 1')
+    return render(
+        request,
+        'home/index.html'
+    )
