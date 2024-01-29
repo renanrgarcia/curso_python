38c562fe6d7572baceea97123e8f6e81b16197aa
Renan <renanrgarcia@gmail.com>
2024-01-29 13:34:21 -0300

    Usando a mesma URL de forma estática e dinâmica

--- secao9-djangoNoPython/aula127_ola_django/blog/urls.py
+++ secao9-djangoNoPython/aula127_ola_django/blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 # https://docs.djangoproject.com/en/5.0/topics/http/urls/
 urlpatterns = [
     path('', views.blog, name='home'),
-    path('post/<int:id>', views.post, name='post'),
+    path('<int:id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
