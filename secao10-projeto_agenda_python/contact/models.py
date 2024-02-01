658ca2027bf9495c478d1010f4d1faab8e5869af
Renan Garcia <renanrgarcia@gmail.com>
2024-02-01 07:01:10 -0300

    Customizando as opções admin.ModelAdmin (Área administrativa do Django)

--- secao10-projeto_agenda_python/contact/models.py
+++ secao10-projeto_agenda_python/contact/models.py
@@ -11,7 +11,7 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
-    fisrt_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=11)
     email = models.EmailField(max_length=254, blank=True)
@@ -19,4 +19,4 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
 
     def __str__(self) -> str:
-        return f'{self.fisrt_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
